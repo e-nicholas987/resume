460278f628db53e8557ca3c1b6da06312e96241e
e.nicholas987 <i.emeka74@yahoo.com>
2022-05-10 18:37:49 +0100

    portfolio update

--- main.js
+++ main.js
@@ -1,18 +1,57 @@
 
-const openMenu = () => {
+function openMenu() {
     document.getElementById('aside').style.transform = 'none';
     document.getElementById('main').style.transform = 'translateX(300px)';
     document.getElementById('overlay').style.display = 'block';
     document.getElementById('close').style.display = 'block';
+    document.getElementsByClassName('wrapper')[0].style.overflow = 'hidden'
     main.style.overflowY = 'hidden';
-    document.body.style.overflow = 'hidden';
 }
 
-const closeMenu = () => {
+
+function closeMenu() {
+    if(window.innerWidth > 768) return
     document.getElementById('aside').style.transform = 'translateX(-400px)';
-    document.getElementById('main').style.transform = 'translateX(0)';
+    document.getElementById('main').style.transform = 'translateX(0px)';
     document.getElementById('overlay').style.display = 'none';
     document.getElementById('close').style.display = 'none';
     main.style.overflowY = 'auto';
-    document.body.style.overflow = 'auto';
+    document.getElementsByClassName('wrapper')[0].style.overflow = 'auto'
 }
+
+const elementInView = (el, dividend = 1) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+        elementTop <=
+        (window.innerHeight || document.documentElement.clientHeight) / dividend
+    );
+};
+
+const elementOutofView = (el) => {
+    const elementTop = el.getBoundingClientRect().top;
+  
+    return (
+      elementTop > (window.innerHeight || document.documentElement.clientHeight)
+    );
+  };
+
+
+function animate() {
+    document.querySelectorAll('.fade-in').forEach(element => {
+        const section = element.closest('section')
+        if (elementInView(section, 1.25)) {
+            element.style.top = ('0')
+            element.style.opacity = ('1')
+        }
+        else if (elementOutofView(section)) {
+            element.style.top = ('100px')
+            element.style.opacity = ('0')
+        }
+    })
+
+}
+
+
+document.getElementById('main').addEventListener('scroll', animate);
+window.addEventListener('load', animate)
